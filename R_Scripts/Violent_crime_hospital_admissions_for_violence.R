348f5b3b80ba4c2a09f402c71900288a1e5a34d3
Rich Tyler <rich.tyler.2018@hotmail.com>
2022-08-30 17:09:11 +0100

    Violence work started

---
+++ R_Scripts/Violent_crime_hospital_admissions_for_violence.R
@@ -0,0 +1,19 @@
+
+# Indicator B12a - Violent Crime hospital admissions for violence (including sexual violence)
+
+# The number of emergency hospital admissions for violence (external causes: ICD-10 codes X85 to Y09). Directly age standardised rate per 100,000 population.
+
+# The number of first finished emergency admission episodes in patients (episode number = 1, admission method starts with 2), with a recording of violent crime classified by diagnosis code (X85 to Y09 occurring in any diagnosis position, primary or secondary) in financial year in which episode ended.
+
+# Regular and day attenders have been excluded. Admissions are only included if they have a valid Local Authority code. Regions are the sum of the Local Authorities. England is the sum of all Local Authorities and admissions coded as U (England NOS).
+
+# 3 year pooled ONS Mid-year population estimates
+
+# HES inpatient data are generally considered to be complete and robust. However, there may be a question regarding the quality of external cause coding and differences in admission thresholds. There may be variation between Trusts in the way hospital admissions are coded. There may be variation in data recording completeness. Information could potentially be missing in the admission episode record but added instead to a subsequent episode record. In addition, some transfers which are also coded as episode order 1 (epiorder 1) and emergency could lead to double counting.
+
+# Notes	Where the observed total number of admissions is less than 10, the rates have been suppressed as there are too few admissions to calculate directly standardised rates reliably. The cut-off has been reduced from 25, following research commissioned by PHE and in preparation for publication which shows DSRs and their confidence intervals are robust whenever the count is at least 10.
+
+
+# Crime data at Community Safety Partnership area level -
+
+# https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/crimeandjustice/datasets/recordedcrimedatabycommunitysafetypartnershiparea/yearendingmarch2022/cspyemar22final.xlsx
