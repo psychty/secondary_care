95d1bac98feec54d655e8282fa1d3f90ee2fd2a2
Rich Tyler <rich.tyler.2018@hotmail.com>
2023-11-17 12:18:28 +0000

    pivot added

--- SQL_snippets/ICT_Coastal_work_LSOA/Pivot_emergency_admissions_falls_by_age.sql
+++ SQL_snippets/ICT_Coastal_work_LSOA/Pivot_emergency_admissions_falls_by_age.sql
@@ -1,8 +1,31 @@
--- This is the crude rate of hospital admissions caused by unintentional and deliberate injuries in children aged under 5 years per 10,000 resident population aged under 5 years.
+-- Emergency admissions for falls injuries classified by primary diagnosis code (ICD10 code S00 to T98) and external cause (ICD10 code W00 to W19) and an emergency admission code (episode order number equals 1, admission method starts with 2) . Age at admission 65 and over.
 
--- The number of finished emergency admissions (episode number equals 1, admission method starts with 2), with one or more codes for injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36) in any diagnostic field position, in children (aged 0 to 4 years). Admissions are only included if they have a valid Local Authority code. Regions are the sum of the Local Authorities. England is the sum of all Local Authorities and admissions coded as U (England NOS). Admissions that only include T80 to T98 or Y40 to T98, quality of care issues, in any field are excluded.
+-- Regular and day attenders have been excluded. Admissions are only included if they have a valid Local Authority code. Regions are the sum of the Local Authorities. England is the sum of all Local Authorities and admissions coded as U (England NOS).
 
--- LSOA x Age pivoted by financial years 2018/19 - 2022/23 
+SELECT FYEAR AS Financial_year, SUM(FAE) AS Admission_episodes
+FROM hdis_10years.hes_apc_2122_hdis_10years
+-- Finished episodes only (3)
+WHERE EPISTAT = '3'
+-- Admission episode
+AND FAE = 1
+-- Ordinary admission (1), day case (2) or maternity (5)
+AND CLASSPAT IN ('1','2','5')
+-- Emergency admissions
+AND ADMIMETH LIKE ('2%')
+-- First episode of a spell
+AND EPIORDER = 1
+-- causes and conditions 
+-- external cause is a fall
+AND CAUSE_3 RLIKE ('W[01]')
+-- primary diagnosis is injury 
+AND DIAG_3_01 RLIKE ('S|T') 
+-- England and Unknown LAD only
+AND RESLADST_ONS RLIKE ('E|U')
+-- Aged 65+
+AND STARTAGE_CALC >= 65
+GROUP BY Financial_year
+
+--Emergency admissions for falls injuries classified by primary diagnosis code (ICD10 code S00 to T98) and external cause (ICD10 code W00 to W19) and an emergency admission code (episode order number equals 1, admission method starts with 2) . Age at admission 65 and over.
 SELECT *
 FROM (
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
@@ -18,10 +41,10 @@ AND ADMIMETH LIKE ('2%')
 -- First episode of a spell
 AND EPIORDER = 1
 -- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
+-- external cause is a fall
+AND CAUSE_3 RLIKE ('W[01]')
+-- primary diagnosis is injury 
+AND DIAG_3_01 RLIKE ('S|T') 
 -- England and Unknown LAD only
 AND RESLADST_ONS RLIKE ('E|U')
 UNION ALL -- this joins the results of queries together
@@ -38,13 +61,13 @@ AND ADMIMETH LIKE ('2%')
 -- First episode of a spell
 AND EPIORDER = 1
 -- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
+-- external cause is a fall
+AND CAUSE_3 RLIKE ('W[01]')
+-- primary diagnosis is injury 
+AND DIAG_3_01 RLIKE ('S|T') 
 -- England and Unknown LAD only
 AND RESLADST_ONS RLIKE ('E|U')
-UNION ALL-- this joins the results of queries together
+UNION ALL
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
 FROM hdis_10years.hes_apc_2021_hdis_10years
 -- Finished episodes only (3)
@@ -58,10 +81,10 @@ AND ADMIMETH LIKE ('2%')
 -- First episode of a spell
 AND EPIORDER = 1
 -- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
+-- external cause is a fall
+AND CAUSE_3 RLIKE ('W[01]')
+-- primary diagnosis is injury 
+AND DIAG_3_01 RLIKE ('S|T') 
 -- England and Unknown LAD only
 AND RESLADST_ONS RLIKE ('E|U')
 UNION ALL -- this joins the results of queries together
@@ -78,13 +101,13 @@ AND ADMIMETH LIKE ('2%')
 -- First episode of a spell
 AND EPIORDER = 1
 -- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
+-- external cause is a fall
+AND CAUSE_3 RLIKE ('W[01]')
+-- primary diagnosis is injury 
+AND DIAG_3_01 RLIKE ('S|T') 
 -- England and Unknown LAD only
 AND RESLADST_ONS RLIKE ('E|U')
-UNION ALL -- this joins the results of queries together
+UNION ALL
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
 FROM hdis_10years.hes_apc_2223_hdis_10years
 -- Finished episodes only (3)
@@ -98,10 +121,10 @@ AND ADMIMETH LIKE ('2%')
 -- First episode of a spell
 AND EPIORDER = 1
 -- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
+-- external cause is a fall
+AND CAUSE_3 RLIKE ('W[01]')
+-- primary diagnosis is injury 
+AND DIAG_3_01 RLIKE ('S|T') 
 -- England and Unknown LAD only
 AND RESLADST_ONS RLIKE ('E|U')
    )
--- SQL_snippets/ICT_Coastal_work_LSOA/Pivot_emergency_admissions_pneumonia_by_age.sql
+++ SQL_snippets/ICT_Coastal_work_LSOA/Pivot_emergency_admissions_pneumonia_by_age.sql
@@ -1,109 +1,97 @@
--- This is the crude rate of hospital admissions caused by unintentional and deliberate injuries in children aged under 5 years per 10,000 resident population aged under 5 years.
+-- Emergency admissions to hospital where the primary diagnosis is pneumonia (ICD-10 codes J12-J18). Directly age standardised rate per 100,000 population (standardised to the European standard population)
 
--- The number of finished emergency admissions (episode number equals 1, admission method starts with 2), with one or more codes for injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36) in any diagnostic field position, in children (aged 0 to 4 years). Admissions are only included if they have a valid Local Authority code. Regions are the sum of the Local Authorities. England is the sum of all Local Authorities and admissions coded as U (England NOS). Admissions that only include T80 to T98 or Y40 to T98, quality of care issues, in any field are excluded.
+-- This is at CCG of responsibility geography so may be difficult to match exactly with our HES queries. All values have been recalculated for the 2021 configuration of CCGs. Specialist commissioning codes are excluded (what are these?)
 
--- LSOA x Age pivoted by financial years 2018/19 - 2022/23 
+-- To check England all ages 2020/21 figure (numerator) (sum of CCGs) - I can get to 138,559 from HES compared to 134,999 from fingertips. If we use residence geographies we'll have our own England value - I think we're close enough. 
+SELECT FYEAR AS Financial_year, SUM(FAE) AS Admission_episodes
+FROM hdis_10years.hes_apc_2021_hdis_10years
+-- Finished episodes (3)
+WHERE EPISTAT = '3'
+-- Emergency (non-elective) admission
+AND ADMIMETH LIKE ('2%')
+-- Finished admission episode 
+AND FAE = 1
+-- First episode of spell
+AND EPIORDER = 1
+-- Conditions and causes
+-- Primary diagnosis pneumonia (J12-J18)
+AND DIAG_3_01 RLIKE ('J1[2-8]')
+-- England CCG excluding unknown ('59999')
+AND CCG_RESPONSIBILITY NOT RLIKE ('59999')
+GROUP BY Financial_year
+
+-- Emergency admissions to hospital where the primary diagnosis is pneumonia (ICD-10 codes J12-J18). Directly age standardised rate per 100,000 population (standardised to the European standard population)
 SELECT *
 FROM (
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
 FROM hdis_10years.hes_apc_1819_hdis_10years
--- Finished episodes only (3)
+-- Finished episodes (3)
 WHERE EPISTAT = '3'
--- Admission episode
-AND FAE = 1
--- Ordinary admission (1), day case (2) or maternity (5)
-AND CLASSPAT IN ('1','2','5')
--- Emergency admissions
+-- Emergency (non-elective) admission
 AND ADMIMETH LIKE ('2%')
--- First episode of a spell
+-- Finished admission episode 
+AND FAE = 1
+-- First episode of spell
 AND EPIORDER = 1
--- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
--- England and Unknown LAD only
-AND RESLADST_ONS RLIKE ('E|U')
+-- Conditions and causes
+-- Primary diagnosis pneumonia (J12-J18)
+AND DIAG_3_01 RLIKE ('J1[2-8]')
 UNION ALL -- this joins the results of queries together
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
 FROM hdis_10years.hes_apc_1920_hdis_10years
--- Finished episodes only (3)
+-- Finished episodes (3)
 WHERE EPISTAT = '3'
--- Admission episode
-AND FAE = 1
--- Ordinary admission (1), day case (2) or maternity (5)
-AND CLASSPAT IN ('1','2','5')
--- Emergency admissions
+-- Emergency (non-elective) admission
 AND ADMIMETH LIKE ('2%')
--- First episode of a spell
+-- Finished admission episode 
+AND FAE = 1
+-- First episode of spell
 AND EPIORDER = 1
--- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
--- England and Unknown LAD only
-AND RESLADST_ONS RLIKE ('E|U')
+-- Conditions and causes
+-- Primary diagnosis pneumonia (J12-J18)
+AND DIAG_3_01 RLIKE ('J1[2-8]')
 UNION ALL-- this joins the results of queries together
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
 FROM hdis_10years.hes_apc_2021_hdis_10years
--- Finished episodes only (3)
+-- Finished episodes (3)
 WHERE EPISTAT = '3'
--- Admission episode
-AND FAE = 1
--- Ordinary admission (1), day case (2) or maternity (5)
-AND CLASSPAT IN ('1','2','5')
--- Emergency admissions
+-- Emergency (non-elective) admission
 AND ADMIMETH LIKE ('2%')
--- First episode of a spell
+-- Finished admission episode 
+AND FAE = 1
+-- First episode of spell
 AND EPIORDER = 1
--- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
--- England and Unknown LAD only
-AND RESLADST_ONS RLIKE ('E|U')
+-- Conditions and causes
+-- Primary diagnosis pneumonia (J12-J18)
+AND DIAG_3_01 RLIKE ('J1[2-8]')
 UNION ALL -- this joins the results of queries together
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
 FROM hdis_10years.hes_apc_2122_hdis_10years
--- Finished episodes only (3)
+-- Finished episodes (3)
 WHERE EPISTAT = '3'
--- Admission episode
-AND FAE = 1
--- Ordinary admission (1), day case (2) or maternity (5)
-AND CLASSPAT IN ('1','2','5')
--- Emergency admissions
+-- Emergency (non-elective) admission
 AND ADMIMETH LIKE ('2%')
--- First episode of a spell
+-- Finished admission episode 
+AND FAE = 1
+-- First episode of spell
 AND EPIORDER = 1
--- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
--- England and Unknown LAD only
-AND RESLADST_ONS RLIKE ('E|U')
+-- Conditions and causes
+-- Primary diagnosis pneumonia (J12-J18)
+AND DIAG_3_01 RLIKE ('J1[2-8]')
 UNION ALL -- this joins the results of queries together
     SELECT LSOA11 AS LSOA11CD, CASE WHEN FYEAR = 1819 THEN 'FY_201819' WHEN FYEAR = 1920 THEN 'FY_201920' WHEN FYEAR = 2021 THEN 'FY_202021' WHEN FYEAR = 2122 THEN 'FY_202122' WHEN FYEAR = 2223 THEN 'FY_202223' ELSE FYEAR END AS Financial_year, CASE WHEN STARTAGE_CALC BETWEEN 0 AND 4 THEN '0-4 years' WHEN STARTAGE_CALC BETWEEN 5 AND 9 THEN '5-9 years' WHEN STARTAGE_CALC BETWEEN 10 AND 14 THEN '10-14 years' WHEN STARTAGE_CALC BETWEEN 15 AND 19 THEN '15-19 years' WHEN STARTAGE_CALC BETWEEN 20 AND 24 THEN '20-24 years' WHEN STARTAGE_CALC BETWEEN 25 AND 29 THEN '25-29 years' WHEN STARTAGE_CALC BETWEEN 30 AND 34 THEN '30-34 years' WHEN STARTAGE_CALC BETWEEN 35 AND 39 THEN '35-39 years' WHEN STARTAGE_CALC BETWEEN 40 AND 44 THEN '40-44 years' WHEN STARTAGE_CALC BETWEEN 45 AND 49 THEN '45-49 years' WHEN STARTAGE_CALC BETWEEN 50 AND 54 THEN '50-54 years' WHEN STARTAGE_CALC BETWEEN 55 AND 59 THEN '55-59 years' WHEN STARTAGE_CALC BETWEEN 60 AND 64 THEN '60-64 years' WHEN STARTAGE_CALC BETWEEN 65 AND 69 THEN '65-69 years' WHEN STARTAGE_CALC BETWEEN 70 AND 74 THEN '70-74 years' WHEN STARTAGE_CALC BETWEEN 75 AND 79 THEN '75-79 years' WHEN STARTAGE_CALC BETWEEN 80 AND 84 THEN '80-84 years' WHEN STARTAGE_CALC BETWEEN 85 AND 89 THEN '85-89 years' WHEN STARTAGE_CALC BETWEEN 90 AND 120 THEN '90+ years' ELSE 'Unknown' END AS Age_group, FAE
 FROM hdis_10years.hes_apc_2223_hdis_10years
--- Finished episodes only (3)
+-- Finished episodes (3)
 WHERE EPISTAT = '3'
--- Admission episode
-AND FAE = 1
--- Ordinary admission (1), day case (2) or maternity (5)
-AND CLASSPAT IN ('1','2','5')
--- Emergency admissions
+-- Emergency (non-elective) admission
 AND ADMIMETH LIKE ('2%')
--- First episode of a spell
+-- Finished admission episode 
+AND FAE = 1
+-- First episode of spell
 AND EPIORDER = 1
--- causes and conditions 
--- injuries and other adverse effects of external causes (ICD 10: S00 to T79 and or V01 to Y36)
-AND (DIAG_3_CONCAT RLIKE ('S|V|W|X') 
-AND DIAG_3_CONCAT RLIKE ('T[0-7]') 
-OR DIAG_3_CONCAT RLIKE ('Y[0-2]|Y3[0-6]')) 
--- England and Unknown LAD only
-AND RESLADST_ONS RLIKE ('E|U')
+-- Conditions and causes
+-- Primary diagnosis pneumonia (J12-J18)
+AND DIAG_3_01 RLIKE ('J1[2-8]')
    )
     PIVOT
     (
